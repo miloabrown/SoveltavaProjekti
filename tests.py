6f50b784fcfd55cfa3249a47c57de4dcee46f8f2
--replace-all <mabrow@utu.fi>
2021-04-19 11:16:06 +0300

    Add SearchAlgorithms

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ import unittest
 import algorithmAnalysis as aA
 import sortingAlgo as sA
 import searchAlgo as seA
+import numpy as np
 
 
 
@@ -14,8 +15,19 @@ class testLists(unittest.TestCase):
         lista1 = aA.getSorted()
         self.assertEqual(len(lista1), 100000)
 
-
-
+    def testLinearSearch(self):
+        """
+        Testataan Lineaarihaku algoritmin toimivuus
+        """
+        lista1 = np.arange(5)
+        self.assertTrue(seA.algoritmi1(lista1,4),4)
+    
+    def testBinarySearch(self):
+        """
+        Testataan Binäärihaku algoritmin toimivuus
+        """
+        lista1 = np.arange(5)
+        self.assertTrue(seA.algoritmi2(lista1,4),4)
 
 if __name__ == "__main__":
     unittest.main()
